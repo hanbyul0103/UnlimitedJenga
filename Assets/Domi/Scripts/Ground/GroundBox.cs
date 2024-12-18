a9059c0902ce4beb6dd50928d80bf875798006d1
qazplm5602 <t01048187045@gmail.com>
2024-12-18 15:57:28 +0900

    fix: ground detect fly

--- Assets/Domi/Scripts/Ground/GroundBox.cs
+++ Assets/Domi/Scripts/Ground/GroundBox.cs
@@ -30,7 +30,6 @@ public class GroundBox : MonoBehaviour
             }
         }
 
-        print($"maxHeight: {maxHeight}");
         return maxHeight;
     }
     
--- Assets/Domi/Scripts/Ground/GroundDetectBlock.cs
+++ Assets/Domi/Scripts/Ground/GroundDetectBlock.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GroundDetectBlock : MonoBehaviour, IBlockOutHandler
@@ -7,27 +9,63 @@ public class GroundDetectBlock : MonoBehaviour, IBlockOutHandler
     public bool IsGrounded => groundBox != null;
     [SerializeField] private float allowDistance = 0.1f;
 
-    private SpriteRenderer spriteRenderer;
     private Collider2D _collider;
+    private List<GroundDetectBlock> bottomBlocks = new();
+    private Dictionary<GroundDetectBlock, System.Action<GroundBox>> blockEventHandlers = new();
+
+    public event System.Action<GroundBox> OnChangeGround;
     
     private void Awake() {
-        spriteRenderer = GetComponent<SpriteRenderer>();
         _collider = GetComponent<Collider2D>();
     }
 
     public void GroundToutch(GroundBox box) {
         groundBox = box;
         box.AddBlock(this);
+
+        OnChangeGround?.Invoke(box);
     }
     
     public void GroundExit() {
         groundBox.RemoveBlock(this);
         groundBox = null;
+        OnChangeGround?.Invoke(null);
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if (IsGrounded && other.gameObject.TryGetComponent(out GroundDetectBlock block) && !block.IsGrounded) {
-            block.GroundToutch(groundBox);
+        if (other.gameObject.TryGetComponent(out GroundDetectBlock block)) {
+            if (IsGrounded && !block.IsGrounded) {
+                block.GroundToutch(groundBox);
+            }
+            bottomBlocks.Add(block);
+
+            // 이벤트트
+            blockEventHandlers[block] = (value) => OtherBlockChangeGround(block, value);
+            block.OnChangeGround += blockEventHandlers[block];
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.TryGetComponent(out GroundDetectBlock block)) {
+            block.OnChangeGround -= blockEventHandlers[block];
+            blockEventHandlers.Remove(block);
+
+            bottomBlocks.Remove(block);
+        }
+    }
+
+    private void OtherBlockChangeGround(GroundDetectBlock block, GroundBox ground) {
+
+        // 붙어있는 블럭이 더 아래 있고 땅에 닿으면
+        if (block.transform.position.y - block.GetHeight() / 2f < transform.position.y - GetHeight() / 2f) {
+            if (ground && !IsGrounded)
+                GroundToutch(ground);
+            else if (IsGrounded) { // 땅에 없음
+                // 붙어있는 블럭 다 찾음
+                bool detectGround = bottomBlocks.Any(b => b.IsGrounded);
+                if (!detectGround) // 그래도 붙어있는 블럭들 중에 땅이 다 안닿여있음
+                    GroundExit();
+            }
         }
     }
 
