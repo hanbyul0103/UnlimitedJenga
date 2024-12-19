c7574e65ed36ec7a21d842ea8841ed65870694a2
qazplm5602 <t01048187045@gmail.com>
2024-12-20 01:07:48 +0900

    feat: EarthquakeAttackWave

---
+++ Assets/Domi/Scripts/Attack/EarthquakeAttackWave.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UnityEngine;
+
+public class EarthquakeAttackWave : WaveAttackBase
+{
+    [SerializeField] float shakeStrength = 5;
+    [SerializeField] private float shakeSpeed = 2f;
+    private WaveAttackCam cam;
+    private float currentStrength = 0.0f;
+
+    Rigidbody2D[] shakeBlocks;
+
+    private void Awake() {
+        cam = FindAnyObjectByType<WaveAttackCam>();
+    }
+    
+    public override void AttackStart(Vector2 rangeY)
+    {
+        Block[] blocks = FindObjectsByType<Block>(FindObjectsSortMode.None);
+        shakeBlocks = blocks.Where(v => v.transform.position.y >= rangeY.x).Select(v => v.GetComponent<Rigidbody2D>()).ToArray();
+
+        currentStrength = shakeStrength;
+        StartCoroutine(EarthquakeEffect());
+    }
+    
+    IEnumerator EarthquakeEffect() {
+        yield return new WaitForSeconds(18f);
+        DOTween.To(() => currentStrength, x => currentStrength = x, 0, 2f);
+    }
+
+    private void Update() {
+        cam.SetNoise(currentStrength, currentStrength);
+        float x = Mathf.Cos(Time.time * 360 * Mathf.Deg2Rad * shakeSpeed) * shakeStrength;
+
+        foreach (var block in shakeBlocks) {
+            block.AddForce(Vector2.right * x);
+        }
+    }
+}
