ceaeb84f24e68e02d5624cb77d2dd8436a5bf5d4
hanbyul0103 <all007b@naver.com>
2024-12-20 01:50:50 +0900

    play sound

--- Assets/HB/01.Scripts/Block/Block.cs
+++ Assets/HB/01.Scripts/Block/Block.cs
@@ -58,7 +58,9 @@ public abstract class Block : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Block"))
+        if (collision.gameObject.CompareTag("Ground")
+            || collision.gameObject.CompareTag("Block")
+            || collision.gameObject.CompareTag("Logo"))
         {
             SoundManager.Instance.PlaySFX("BlockFall");
         }
