b68a6ffcfdab372c78a3ae450ed30091bccbbf57
qazplm5602 <t01048187045@gmail.com>
2024-12-19 19:18:56 +0900

    feat: 강풍올백

--- Assets/Domi/Scripts/Attack/WaveAttackBase.cs
+++ Assets/Domi/Scripts/Attack/WaveAttackBase.cs
@@ -2,7 +2,18 @@ using UnityEngine;
 
 public abstract class WaveAttackBase : MonoBehaviour
 {
-    public abstract void AttackStart(Vector2 rangeX);
+    public abstract void AttackStart(Vector2 rangeY);
+    public Vector2 GetScreenSideX(float margin) {
+        float leftX = Camera.main.ViewportToWorldPoint(new Vector3(Camera.main.rect.xMin, 0, 0)).x - margin;
+        float rightX = Camera.main.ViewportToWorldPoint(new Vector3(Camera.main.rect.xMax, 0, 0)).x + margin;
+        return new Vector2(leftX, rightX);        
+    }
 
     // OnDestroy 되면 끝난거임
+    
+
+    public static Quaternion LookRotation2D(Vector2 direction)
+    {
+        return Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+    }
 }
---
+++ Assets/Domi/Scripts/Attack/WaveAttackWinde.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using UnityEngine;
+
+public class WaveAttackWinde : WaveAttackBase
+{
+    [SerializeField] private ParticleSystem windEffect;
+    [SerializeField] private LayerMask blockLayer;
+    [SerializeField] private float pushValue = 50f;
+
+    ParticleSystem particle;
+    Vector2 rangeY;
+    private Vector2 direction;
+
+    public override void AttackStart(Vector2 val)
+    {
+        rangeY = val;
+        particle = Instantiate(windEffect, Vector3.zero, Quaternion.identity);
+        StartCoroutine(WineEffect());
+    }
+
+    private void OnDestroy() {
+        if (particle)
+            Destroy(particle.gameObject);
+    }
+
+    IEnumerator WineEffect() {
+        int loop = 3;
+        float timer;
+        
+        while (loop-- > 0) {
+            yield return new WaitForSeconds(3f); // 3초 wait..
+            SetRandomDirectionAndPos();
+            particle.Play();
+
+            timer = 7f; // 7초동안
+            while (timer > 0) {
+                timer -= Time.deltaTime;
+                BlocksAddForce();
+                yield return null;
+            }
+
+            particle.Stop();
+            particle.Clear();
+        }
+    }
+
+    private void SetRandomDirectionAndPos() {
+        bool left = Random.Range(0, 2) == 0;
+        Vector2 rangeX = GetScreenSideX(3f);
+        Vector2 rangeCenter = new Vector2((rangeX.x + rangeX.y) / 2, (rangeY.x + rangeY.y) / 2);
+
+        Vector2 pos = new Vector2(left ? rangeX.x : rangeX.y, Random.Range(rangeY.x, rangeY.y));
+        direction = (rangeCenter - pos).normalized;
+
+        particle.transform.position = pos;
+        particle.transform.rotation = LookRotation2D(direction);
+    }
+    
+    private void BlocksAddForce() {
+        float size = Mathf.Abs(rangeY.y - rangeY.x);
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(particle.transform.position, new Vector2(size, 10), particle.transform.eulerAngles.z, direction, 999f, blockLayer);
+        print($"hits: {hits.Length}");
+        
+        foreach (var hit in hits) {
+            Rigidbody2D rb = hit.collider.GetComponent<Rigidbody2D>();
+            if (rb) {
+                rb.AddForce(direction * pushValue);
+            }
+        }
+    }
+
+    
+}
